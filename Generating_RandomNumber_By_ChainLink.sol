0827d6cbbd5ac3565caa3d623f32097ed7a8844e
Haris Ali <haris.aali9211@gmail.com>
2022-11-13 01:44:22 +0500

    Update Generating_RandomNumber_By_ChainLink.sol

--- Generating_RandomNumber_By_ChainLink.sol
+++ Generating_RandomNumber_By_ChainLink.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <=0.9.0;
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+// import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 contract RandomNumber is VRFConsumerBase {
     bytes32 internal keyHash;
     uint internal fees;
