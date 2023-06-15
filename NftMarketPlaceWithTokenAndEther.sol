47bf84270df939925e99fca56ea1d1869fe99f98
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2023-06-15 20:40:22 +0500

    Update NftMarketPlaceWithTokenAndEther.sol

--- NftMarketPlaceWithTokenAndEther.sol
+++ NftMarketPlaceWithTokenAndEther.sol
@@ -19,7 +19,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 error SimilarToCurrentBaseURI(string currentBaseURI);
 error PriceNotMatched(uint256 price);
 error InvalidTypeOrUserType(uint256 _type);
-error TimeEndedForPreSale(uint256 _blockTime);
+error TimeEndedForSale(uint256 _blockTime);
 error WrongMethodSelected(uint256 _method);
 error NFTLimitReached(uint256 _mintIndex);
 error TransferFailed();
@@ -39,8 +39,8 @@ interface IBEP20 {
 
 contract Practice is ERC721Enumerable, Ownable {
     struct User {
-        uint256 userTyprWhiteList;
-        uint256 userTyprNonWhiteList;
+        uint256 userTypeWhiteList;
+        uint256 userTypeNonWhiteList;
         uint256 nftCountPreSale;
         uint256 nftCountSale;
     }
@@ -64,8 +64,8 @@ contract Practice is ERC721Enumerable, Ownable {
     event NFTMinted(address indexed user, uint256 indexed tokenId);
     event UserSaved(
         address indexed userAddress,
-        uint256 userTyprWhiteList,
-        uint256 userTyprNonWhiteList,
+        uint256 userTypeWhiteList,
+        uint256 userTypeNonWhiteList,
         uint256 nftCountPreSale,
         uint256 nftCountSale
     );
@@ -73,20 +73,20 @@ contract Practice is ERC721Enumerable, Ownable {
     constructor(address token) ERC721("Practice", "PT") {
         TokenAddress = IBEP20(token);
         blockTime = block.timestamp;
-        preSaleTimestamp = blockTime + 1800; // Adding 30 minutes (30 * 60 seconds)
-        saleTimestamp = preSaleTimestamp + 3600; // Adding 60 minutes (60 * 60 seconds)
+        preSaleTimestamp = blockTime + 60; //1800; // Adding 30 minutes (30 * 60 seconds)
+        saleTimestamp = preSaleTimestamp + 120; //3600; // Adding 60 minutes (60 * 60 seconds)
     }
 
     function saveUser(
         address _userAddress,
-        uint256 _userTyprWhiteList,
-        uint256 _userTyprNonWhiteList,
+        uint256 _userTypeWhiteList,
+        uint256 _userTypeNonWhiteList,
         uint256 _nftCountPreSale,
         uint256 _nftCountSale
     ) public {
         User memory newUser = User(
-            _userTyprWhiteList,
-            _userTyprNonWhiteList,
+            _userTypeWhiteList,
+            _userTypeNonWhiteList,
             _nftCountPreSale,
             _nftCountSale
         );
@@ -94,161 +94,237 @@ contract Practice is ERC721Enumerable, Ownable {
 
         emit UserSaved(
             _userAddress,
-            _userTyprWhiteList,
-            _userTyprNonWhiteList,
+            _userTypeWhiteList,
+            _userTypeNonWhiteList,
             _nftCountPreSale,
             _nftCountSale
         );
     }
 
-    function checkIfSenderExistsInWhiteList() public view returns (bool) {
-        return
-            Records[msg.sender].userTyprWhiteList != 0 ||
-            Records[msg.sender].userTyprWhiteList != 0;
-    }
-
-    function buyNFT(
-        string calldata data,
-        uint256 _type, // here _type 1 is preSale and _type 2 is sale
-        uint256 _method //here _method 1 is to buy nft from eth and _method w is to buy nft from token
-    ) public payable {
-        if (checkIfSenderExistsInWhiteList() && _type == 1) {
-            if (block.timestamp <= preSaleTimestamp) {
-                if (_method == 1) {
-
-                    if(Records[msg.sender].nftCountPreSale < 1){
-                        revert UserNFTLimitReached();
-                    }
-
-                    if (msg.value < preSalePrice) {
-                        revert PriceNotMatched(preSalePrice);
-                    }
-
-                    uint256 mintIndex = totalSupply() + 50;
-
-                    if (mintIndex > maxSupply) {
-                        revert NFTLimitReached(mintIndex);
-                    }
-
-                    _safeMint(_msgSender(), mintIndex);
-                    _setTokenURI(mintIndex, data);
-
-                    emit NFTMinted(_msgSender(), mintIndex);
-                } else if (_method == 2) {
-
-                    if(Records[msg.sender].nftCountPreSale < 1){
-                        revert UserNFTLimitReached();
-                    }
-
-                    if (msg.value < preSalePriceToken) {    //yhn check karo msg.value say to nhi jay ga tokens
-                        revert PriceNotMatched(preSalePrice);
-                    }
-                    uint256 mintIndex = totalSupply() + 50;
-                    if (mintIndex > maxSupply) {
-                        revert NFTLimitReached(mintIndex);
-                    }
-
-                    bool success = TokenAddress.transferFrom(
-                        msg.sender,
-                        wallet,
-                        preSalePriceToken
-                    );
-                    if (!success) {
-                        revert TransferFailed();
-                    }
-                    _safeMint(_msgSender(), mintIndex);
-                    _setTokenURI(mintIndex, data);
-
-                    emit NFTMinted(_msgSender(), mintIndex);
-                } else {
-                    revert WrongMethodSelected(_method);
-                }
-            } else {
-                revert TimeEndedForPreSale(block.timestamp);
-            }
-        } else if (!checkIfSenderExistsInWhiteList() && _type == 2) {
-             if (block.timestamp <= saleTimestamp) {
-                if (_method == 1) {
-
-                    if(Records[msg.sender].nftCountSale < 3){
-                        revert UserNFTLimitReached();
-                    }
-
-                    if (msg.value < salePrice) {
-                        revert PriceNotMatched(salePrice);
-                    }
-                    uint256 mintIndex = totalSupply() + 50;
-                    if (mintIndex > maxSupply) {
-                        revert NFTLimitReached(mintIndex);
-                    }
-                    _safeMint(_msgSender(), mintIndex);
-                    _setTokenURI(mintIndex, data);
-
-                    emit NFTMinted(_msgSender(), mintIndex);
-                } else if (_method == 2) {  
-
-                     if(Records[msg.sender].nftCountSale < 3){
-                        revert UserNFTLimitReached();
-                    }
-
-                    if (msg.value < salePriceToken) {    //yhn check karo msg.value say to nhi jay ga tokens
-                        revert PriceNotMatched(salePriceToken);
-                    }
-                    uint256 mintIndex = totalSupply() + 50;
-                    if (mintIndex > maxSupply) {
-                        revert NFTLimitReached(mintIndex);
-                    }
-
-                    bool success = TokenAddress.transferFrom(
-                        msg.sender,
-                        wallet,
-                        salePriceToken
-                    );
-                    if (!success) {
-                        revert TransferFailed();
-                    }
-                    _safeMint(_msgSender(), mintIndex);
-                    _setTokenURI(mintIndex, data);
-
-                    emit NFTMinted(_msgSender(), mintIndex);
-                } else {
-                    revert WrongMethodSelected(_method);
-                }
-            } else {
-                revert TimeEndedForPreSale(block.timestamp);
-            }
-            
+    function checkUserType() public view returns (uint256) {
+        if (Records[msg.sender].userTypeWhiteList == 1) {
+            return 1;
+        } else if (Records[msg.sender].userTypeNonWhiteList == 1) {
+            return 2;
         } else {
-            revert InvalidTypeOrUserType(_type);
+            return 3;
         }
-        // if (_opt == 1) {
-        //     if (msg.value < preSalePrice) {
-        //         revert PriceNotMatched(preSalePrice);
-        //     }
-        // } else if (_opt == 2) {
-        //     if (msg.value < preSalePrice) {
-        //         revert PriceNotMatched(preSalePrice);
-        //     }
-        // } else if (_opt == 3) {
-        //     if (msg.value < preSalePrice) {
-        //         revert PriceNotMatched(preSalePrice);
-        //     }
-        // } else if (_opt == 4) {
-        //     if (msg.value < preSalePrice) {
-        //         revert PriceNotMatched(preSalePrice);
-        //     }
-        // } else {
-        //     // revert ArbitrumNFT_InvalidOption(1, 2, 3, 4);
-        // }
-        // uint256 mintIndex = totalSupply() + 10001;
-        // (bool success, ) = payable(owner()).call{value: msg.value}("");
-        // if (!success) {
-        //     // revert ArbitrumNFT_TransferFailed();
-        // }
-        // _safeMint(_msgSender(), mintIndex);
-        // _setTokenURI(mintIndex, data);
-        // emit NFTMinted(_msgSender(), mintIndex);
     }
+
+    function checkSalesTime() public view returns (uint256 boolForTimeStamp) {
+        if (block.timestamp < preSaleTimestamp) {
+            return 1;
+        } else if (block.timestamp < saleTimestamp) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    function checkBalanceOfTokens() public view returns (uint256) {
+        uint256 returnValue = TokenAddress.balanceOf(msg.sender);
+        return returnValue;
+    }
+
+    function buyNftPractice(
+        string calldata data,
+        uint256 _method // method 1 mean
+    ) public payable {
+        uint256 userType = checkUserType();
+        uint256 saleTime = checkSalesTime();
+        uint256 price;
+        uint256 priceToken;
+        uint256 mintIndex = totalSupply() + 50;
+
+        if (userType == 1 && saleTime == 1) {
+            if (Records[msg.sender].nftCountPreSale < 1) {
+                if (mintIndex < maxSupply) {
+                    priceToken = preSalePriceToken;
+                    price = preSalePrice;
+                } else {
+                    revert NFTLimitReached(mintIndex);
+                }
+            } else {
+                revert UserNFTLimitReached();
+            }
+        } else if (userType == 2 && saleTime == 2) {
+            if (Records[msg.sender].nftCountPreSale < 3) {
+                if (mintIndex < maxSupply) {
+                    priceToken = salePriceToken;
+                    price = salePrice;
+                } else {
+                    revert NFTLimitReached(mintIndex);
+                }
+            } else {
+                revert UserNFTLimitReached();
+            }
+        } else if (userType == 3 && saleTime == 2) {
+            User memory newUser = User(0, 1, 0, 0);
+            Records[msg.sender] = newUser;
+            priceToken = salePriceToken;
+            price = salePrice;
+        } else {
+            revert TimeEndedForSale(block.timestamp);
+        }
+
+        if (_method == 1) {
+            if (msg.value < price) {
+                revert PriceNotMatched(price);
+            } else {
+                _safeMint(_msgSender(), mintIndex);
+                _setTokenURI(mintIndex, data);
+
+                emit NFTMinted(_msgSender(), mintIndex);
+            }
+        }
+        if (_method == 2) {
+            uint256 returnValue = TokenAddress.balanceOf(msg.sender);
+
+            if (returnValue < priceToken) {
+                revert PriceNotMatched(priceToken);
+            } else {
+                bool success = TokenAddress.transferFrom(
+                    msg.sender,
+                    wallet,
+                    priceToken
+                );
+                if (!success) {
+                    revert TransferFailed();
+                }
+                _safeMint(_msgSender(), mintIndex);
+                _setTokenURI(mintIndex, data);
+
+                emit NFTMinted(_msgSender(), mintIndex);
+            }
+        }
+    }
+
+    // function buyNftTest(
+    //     string calldata data,
+    //     uint256 _type, // here _type 1 is preSale and _type 2 is sale
+    //     uint256 _method, //here _method 1 is to buy nft from eth and _method 2 is to buy nft from token
+    //     uint256 _amount //token
+    // ) public {
+    //     if (checkIfSenderExistsInWhiteList() && _type == 1) {} else if (
+    //         !checkIfSenderExistsInWhiteList() && _type == 2
+    //     ) {} else {
+    //         revert InvalidTypeOrUserType(_type);
+    //     }
+    // }
+
+    // function buyNFT(
+    //     string calldata data,
+    //     uint256 _type, // here _type 1 is preSale and _type 2 is sale
+    //     uint256 _method, //here _method 1 is to buy nft from eth and _method 2 is to buy nft from token
+    //     uint256 _amount //token
+    // ) public payable {
+    //     if (checkIfSenderExistsInWhiteList() && _type == 1) {
+    //         if (block.timestamp <= preSaleTimestamp) {
+    //             if (_method == 1) {
+    //                 if (Records[msg.sender].nftCountPreSale < 1) {
+    //                     revert UserNFTLimitReached();
+    //                 }
+
+    //                 if (msg.value < preSalePrice) {
+    //                     revert PriceNotMatched(preSalePrice);
+    //                 }
+
+    //                 uint256 mintIndex = totalSupply() + 50;
+
+    //                 // if (mintIndex > maxSupply) {
+    //                 //     revert NFTLimitReached(mintIndex);
+    //                 // }
+
+    //                 _safeMint(_msgSender(), mintIndex);
+    //                 _setTokenURI(mintIndex, data);
+
+    //                 emit NFTMinted(_msgSender(), mintIndex);
+    //             } else if (_method == 2) {
+    //                 if (Records[msg.sender].nftCountPreSale < 1) {
+    //                     revert UserNFTLimitReached();
+    //                 }
+
+    //                 if (_amount < preSalePriceToken) {
+    //                     //yhn check karo msg.value say to nhi jay ga tokens
+    //                     revert PriceNotMatched(preSalePrice);
+    //                 }
+    //                 uint256 mintIndex = totalSupply() + 50;
+    //                 // if (mintIndex > maxSupply) {
+    //                 //     revert NFTLimitReached(mintIndex);
+    //                 // }
+
+    //                 bool success = TokenAddress.transferFrom(
+    //                     msg.sender,
+    //                     wallet,
+    //                     preSalePriceToken
+    //                 );
+    //                 if (!success) {
+    //                     revert TransferFailed();
+    //                 }
+    //                 _safeMint(_msgSender(), mintIndex);
+    //                 _setTokenURI(mintIndex, data);
+
+    //                 emit NFTMinted(_msgSender(), mintIndex);
+    //             } else {
+    //                 revert WrongMethodSelected(_method);
+    //             }
+    //         } else {
+    //             revert TimeEndedForPreSale(block.timestamp);
+    //         }
+    //     } else if (!checkIfSenderExistsInWhiteList() && _type == 2) {
+    //         if (block.timestamp <= saleTimestamp) {
+    //             if (_method == 1) {
+    //                 if (Records[msg.sender].nftCountSale < 3) {
+    //                     revert UserNFTLimitReached();
+    //                 }
+
+    //                 if (msg.value < salePrice) {
+    //                     revert PriceNotMatched(salePrice);
+    //                 }
+    //                 uint256 mintIndex = totalSupply() + 50;
+    //                 // if (mintIndex > maxSupply) {
+    //                 //     revert NFTLimitReached(mintIndex);
+    //                 // }
+    //                 _safeMint(_msgSender(), mintIndex);
+    //                 _setTokenURI(mintIndex, data);
+
+    //                 emit NFTMinted(_msgSender(), mintIndex);
+    //             } else if (_method == 2) {
+    //                 if (Records[msg.sender].nftCountSale < 3) {
+    //                     revert UserNFTLimitReached();
+    //                 }
+
+    //                 if (_amount < salePriceToken) {
+    //                     //yhn check karo msg.value say to nhi jay ga tokens
+    //                     revert PriceNotMatched(salePriceToken);
+    //                 }
+    //                 uint256 mintIndex = totalSupply() + 50;
+    //                 // if (mintIndex > maxSupply) {
+    //                 //     revert NFTLimitReached(mintIndex);
+    //                 // }
+
+    //                 bool success = TokenAddress.transferFrom(
+    //                     msg.sender,
+    //                     wallet,
+    //                     salePriceToken
+    //                 );
+    //                 if (!success) {
+    //                     revert TransferFailed();
+    //                 }
+    //                 _safeMint(_msgSender(), mintIndex);
+    //                 _setTokenURI(mintIndex, data);
+
+    //                 emit NFTMinted(_msgSender(), mintIndex);
+    //             } else {
+    //                 revert WrongMethodSelected(_method);
+    //             }
+    //         } else {
+    //             revert TimeEndedForPreSale(block.timestamp);
+    //         }
+    //     } else {
+    //         revert InvalidTypeOrUserType(_type);
+    //     }
+    // }
 
     function _setTokenURI(uint256 tokenId, string memory _tokenURI)
         internal
